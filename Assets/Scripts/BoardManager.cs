767ade90023c5ef113208644c2460c5f05b5f70b
KomanMehmet <108063848+KomanMehmet@users.noreply.github.com>
2024-11-07 16:07:27 +0300

    commit_add_boardManager

---
+++ Assets/Scripts/BoardManager.cs
@@ -0,0 +1,37 @@
+using System;
+using Unity.Mathematics;
+using UnityEngine;
+
+public class BoardManager : MonoBehaviour
+{
+    [SerializeField] private Transform tilePrefab;
+
+    [SerializeField] private int height = 22;
+    [SerializeField] private int width = 10;
+
+    private void Start()
+    {
+        CreateEmptyBox();
+    }
+
+    private void CreateEmptyBox()
+    {
+        if (tilePrefab != null)
+        {
+            for (int h = 0; h < height; h++)
+            {
+                for (int w = 0; w < width; w++)
+                {
+                    Transform tile = Instantiate(tilePrefab, new Vector3(h, w, 0), quaternion.identity);
+                    tile.name = "x " + h.ToString() + " , " + " w" + w.ToString();
+                    tile.parent = transform;
+                }
+            }
+        }
+        else
+        {
+            print("TilePrefab eklenmedi!");
+        }
+        
+    }
+}
